3bd2148ba3ed376c6a21db030ee1bd7bbdac57f0
Paulwamaria <paulwamaria@gmail.com>
2019-09-15 13:12:06 +0300

    create a defination to update profile picttures

--- app/main/views.py
+++ app/main/views.py
@@ -13,3 +13,15 @@ def index():
     screenplay_pitches = picth.get_pitches('screenplay')
     project_pitches = picth.get_pitches('project')
     return render_template('index.html',title = title,business=business,screenplay = screenplay)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))

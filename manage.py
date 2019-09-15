3774aa089344c806e2905ad3c7f9695b66eae82f
Paulwamaria <paulwamaria@gmail.com>
2019-09-15 12:34:25 +0300

    create the manager shell

---
+++ manage.py
@@ -0,0 +1,18 @@
+from flask_script import Manager , Server
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
+from app.models import User
+
+app=create_app('development')
+
+manager =  Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('runserver',Server(use_debugger=True))
+manager.add_command('db',MigrateCommand)
+
+@manager.shell 
+def add_shell_context():
+    return {"db":db,"User":User}
+
+if __name__=="__main__":
+    manager.run()

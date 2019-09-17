09121a9d5a0d8a1ee659d39b5dfd60b98d040ee2
Paulwamaria <paulwamaria@gmail.com>
2019-09-17 12:19:09 +0300

    Change bootsrtap options

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 
+
 from flask import Flask
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap 
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 
-bootstrap = Bootstrap()
+bootstrap= Bootstrap()
+
 db = SQLAlchemy()
 mail = Mail()
 photos = UploadSet('photos',IMAGES)
@@ -25,6 +27,7 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+  
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
